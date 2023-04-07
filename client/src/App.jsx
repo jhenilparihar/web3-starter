590d19ce1406a207cccca257258921b6a4f7e37a
Jhenil Parihar <jhenilparihar@gmail.com>
2023-04-07 15:11:54 +0530

    setup web3 client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Component } from "react";
+import {
+  loadWeb3,
+  isMetamaskConnected,
+  connectToMetamask,
+  getContract,
+} from "./context/Context";
+import { getAccountAddress } from "./context/Context";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+import ConnectToMetamask from "./components/ConnectToMetamask";
+import Loading from "./components/Loading";
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      metamaskConnected: false,
+      accountAddress: null,
+    };
+  }
+  componentWillMount = async () => {
+    await loadWeb3();
+    await this.loadBlockchainData();
+  };
+
+  loadBlockchainData = async () => {
+    if (await isMetamaskConnected()) {
+      this.setState({ metamaskConnected: true });
+      this.setState({ loading: true });
+      const account = await getAccountAddress();
+      const contract = await getContract();
+      const something = await contract.methods.balanceOf(account).call();
+      console.log(something);
+    } else {
+      this.setState({ metamaskConnected: false });
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.loading ? (
+          <Loading />
+        ) : !this.state.metamaskConnected ? (
+          <ConnectToMetamask connectToMetamask={connectToMetamask} />
+        ) : (
+          <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    );
+  }
 }
 
-export default App
+export default App;
