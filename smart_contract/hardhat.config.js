d208cdd5f2e0ed82d77581bce735c1ceff7d9178
Jhenil Parihar <jhenilparihar@gmail.com>
2023-04-07 12:20:25 +0530

    added celo network

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -12,5 +12,10 @@ module.exports = {
       url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts: [process.env.PRIVATE_KEY],
     },
+    alfajores: {
+      url: "https://alfajores-forno.celo-testnet.org",
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 44787,
+    },
   },
 };
